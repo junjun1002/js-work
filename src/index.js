6d802b7a51129879cf47c9000be9be7f9468e4fe
CANDRGROUP\junya.ueno <junya.ueno@pr.cri.co.jp>
2026-02-19 13:51:43 +0900

    課題0実装

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const users = [
+  { id: 1, name: "Taro", coin: 100, level: 5, lastLoginDaysAgo: 1, items: ["potion"] },
+  { id: 2, name: "Hanako", coin: 300, level: 12, lastLoginDaysAgo: 10, items: ["sword", "potion"] },
+  { id: 3, name: "Saburo", coin: 50, level: 2, lastLoginDaysAgo: 0, items: [] },
+  { id: 4, name: "Yuki", coin: 1200, level: 20, lastLoginDaysAgo: 40, items: ["shield"] },
+];
+
+console.log("users:", users);
